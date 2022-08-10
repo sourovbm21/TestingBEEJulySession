f7914bd7c7d8a96a37f3233e4d97c26b0ba851ea
sourov <sourovbm21@gmail.com>
2022-08-10 19:23:20 +0100

    Please have a look to solution

--- src/main/java/HomeWork/Task.java
+++ src/main/java/HomeWork/Task.java
@@ -1,91 +1,78 @@
 package HomeWork;
 
 public class Task {
-	
-	//Hi all there is a task list 
-	
-	
+
+	// Hi all there is a task list
+
 	/*
 	 * 
 	 * 
-	 * 01.Create a method with no arguments;
-	 * 02.Create a method which return void;
-	 * 03.Create a method which return a boolean value;
-	 * 04.Create a method with one parameter Data type is integer; 
-	 * 05.Create a method with Two parameter Data type is String;
-	 * 06.Create a method with Two parameter Data type is integer and String; 
-	 * 07.Create a method with Three parameter Data type is integer, String , boolean;
-	 * 08.Create a method which accept two integer arguments and return sum of those two integer;
-	 * 09.Create a method which accept two string arguments and return boolean value;
-	 * 10.Print 1 to 10 using while loop;
+	 * 01.Create a method with no arguments; 02.Create a method which return void;
+	 * 03.Create a method which return a boolean value; 04.Create a method with one
+	 * parameter Data type is integer; 05.Create a method with Two parameter Data
+	 * type is String; 06.Create a method with Two parameter Data type is integer
+	 * and String; 07.Create a method with Three parameter Data type is integer,
+	 * String , boolean; 08.Create a method which accept two integer arguments and
+	 * return sum of those two integer; 09.Create a method which accept two string
+	 * arguments and return boolean value; 10.Print 1 to 10 using while loop;
 	 * 
 	 * 
 	 */
-	
 
-	
-	// All of you have to finish below task before next class.(08/08/2022-12.08.2022)
+	// All of you have to finish below task before next
+	// class.(08/08/2022-12.08.2022)
 	/*
-	 * Problem:- 11.
-	 * Given a string and a non-negative int n, return a larger string
-	 * that is n copies of the original string.
-	 * Example below : 
-	  stringTimes("Hi", 2) → "HiHi"
-	  stringTimes("Hi", 3) → "HiHiHi" 
-	  stringTimes("Hi", 1) → "Hi"
+	 * Problem:- 11. Given a string and a non-negative int n, return a larger string
+	 * that is n copies of the original string. Example below : stringTimes("Hi", 2)
+	 * → "HiHi" stringTimes("Hi", 3) → "HiHiHi" stringTimes("Hi", 1) → "Hi"
 	 */
 	public static String problem_11(String s, int a) {
 
-		String newOne ="";
+		String newOne = "";
 		for (int i = 0; i < a; i++) {
 			newOne = newOne + s;
 		}
 		return newOne;
 	}
-	
+
 	/*
-	 * Problem:-12
-	 * Given a string, we'll say that the front is the first 3 chars of the string,
-	 * If the string length is less than 3, the front is whatever is there,
-	 * Return a new string which is 3 copies of the front.
-	 * Example below : 
-       front3("Java") → "JavJavJav"
-       front3("Chocolate") → "ChoChoCho"
-       front3("abc") → "abcabcabc"
+	 * Problem:-12 Given a string, we'll say that the front is the first 3 chars of
+	 * the string, If the string length is less than 3, the front is whatever is
+	 * there, Return a new string which is 3 copies of the front. Example below :
+	 * front3("Java") → "JavJavJav" front3("Chocolate") → "ChoChoCho" front3("abc")
+	 * → "abcabcabc"
 	 */
-	
-	   /*
-	    *Problem:-13
-	    * Given a string, take the last char and return a new string with the last char added at the front and back,
-	    * so "cat" yields "tcatt". The original string will be length 1 or more.
-      backAround("cat") → "tcatt"
-      backAround("Hello") → "oHelloo"
-      backAround("a") → "aaa
-	    */
-	
-	
-	
-	
-	public static void main(String[] args) {
+public static String  problem_Twelve(String str) {
 		
+		String front;
 		
-		
-	System.out.println(problem_11("hello",3));	
-	
-	
-		
-		
+		if (str.length() <= 0 ) {
+			
+			front = str.substring(0,3);
+		}else {
+			front = str;
+		}
+			
+		return front + front + front;
+
+		 
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	/*
+	 * Problem:-13 Given a string, take the last char and return a new string with
+	 * the last char added at the front and back, so "cat" yields "tcatt". The
+	 * original string will be length 1 or more. backAround("cat") → "tcatt"
+	 * backAround("Hello") → "oHelloo" backAround("a") → "aaa
+	 */
+
+	public static void main(String[] args) {  
+
+		// The method will return a new string which i am printing using
+		System.out.println(problem_11("hello", 3));
+		System.out.println(problem_Twelve("ja"));
+
+	}
 	
 
 }
