6c5d8d2577d0faf197fbac7c225072729b101ed8
sourov <sourovbm21@gmail.com>
2022-09-26 18:57:55 +0100

    Adding date formatter

--- src/main/java/Java/ClassOne.java
+++ src/main/java/Java/ClassOne.java
@@ -1,5 +1,8 @@
 package Java;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ClassOne {
 	
 	
@@ -18,6 +21,11 @@ public class ClassOne {
 		int y=300000000;
 		long ph=0000000000000000000000000000000000000000000000000l;
 		String yy="Sourov";
+		
+		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter fm = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(fm.format(now));
 	
 		
 	
